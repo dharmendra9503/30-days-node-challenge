8e137d2156caf741138f2b7956e5e0501063f288
DHARMENDRA PRAJAPATI <dharmendra@bit01.tech>
2024-02-03 16:39:42 +0530

    Day 3 Problem Solved

---
+++ Day3/Day3.js
@@ -0,0 +1,50 @@
+/*
+
+### Problem 3: Execute Command
+
+Problem Statement:
+Create a function `executeCommand(command)` that takes a shell command as input and executes it using the `child_process` module. The function should print the output of the command to the console.
+
+Function Signature:
+function executeCommand(command) {
+    // Implementation
+}
+
+
+Expected Output:
+Command Output:
+File1.txt
+File2.txt
+
+
+Test Cases:
+executeCommand('ls -la');
+// Expected Output: (output of ls -la)
+
+executeCommand('echo "Hello, Node.js!"');
+// Expected Output: Hello, Node.js!
+
+
+*/
+
+
+//Solution
+const { exec } = require('child_process');
+
+function executeCommand(command) {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`Command Output:\n${stdout}`);
+    });
+}
+
+executeCommand('ls -la');
+
+executeCommand('echo "Hello, Node.js!"');
